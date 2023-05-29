522d62ec9ce6a0188aca97427a00bb1d04dea3a1
feranzie <oladaviesferanmi@gmail.com>
2023-05-29 13:19:06 -0700

    bard api add

--- tes.py
+++ tes.py
@@ -1,25 +1,11 @@
-import threading
-import time
+from bardapi import Bard 
 
-# Define the function that continuously updates the variable
-def update_variable():
-    global variable
-    for i in range(10):  # Modify the loop condition as per your requirements
-        # Your logic to update the variable goes here
-        variable = i
-        time.sleep(1)  # Simulating the update process
+from dotenv import load_dotenv
 
-# Define the function that prints the variable value every 30 seconds
-def print_variable():
-    while True:
-        #print("Variable value:", variable) 
-        print(variable) # Replace "variable" with the name of your variable
-        time.sleep(30)  # Wait for 30 seconds before printing again
+# Load the environment variables from .env file
+load_dotenv()
+import os
+token = os.environ.get('BARD_API_KEY')
+print(Bard(token).get_answer("how old is the earth")["content"])
+#print(token)
 
-# Start the update_variable thread
-update_thread = threading.Thread(target=update_variable)
-update_thread.start()
-
-# Start the print_variable thread
-print_thread = threading.Thread(target=print_variable)
-print_thread.start()
