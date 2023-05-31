c020c3ec5c02b4ebc2c9654c23e1bb2ac47eee10
feranzie <oladaviesferanmi@gmail.com>
2023-05-31 08:18:52 -0700

    webpagr

--- tes.py
+++ tes.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 token = os.environ.get('BARD_API_KEY')
-print(Bard(token).get_answer("form a sentence describing whats going on within an image of a school bag, trafficlight, 2 persons and a motorcycle detected")["content"])
+print(Bard(token).get_answer("form a short sentence describing whats going on within an image of a school bag, trafficlight, 2 persons and a motorcycle detected")["content"])
 #print(token)
 
