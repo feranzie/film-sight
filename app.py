7f16fcd10684247b7c068fe12584c5f517136747
feranzie <oladaviesferanmi@gmail.com>
2023-06-25 08:54:59 -0700

    f

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def detect():
     video = request.files['video']
     video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
     print(video)
-    subprocess.run("ls", shell=True)
-    subprocess.run(['python', 'detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename))], shell=True)
+    #subprocess.run("ls", shell=True)
+    subprocess.run(['python', 'detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename)), '--view-img'], shell=True)
 
     # return os.path.join(uploads_dir, secure_filename(video.filename))
     obj = secure_filename(video.filename)
